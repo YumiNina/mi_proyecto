9f4b585c3b26dda5808448d53f2ee007448a7f7b
mayumi nina <Mayunina55@gmail.com>
2022-12-02 14:53:39 -0400

    logout

--- lib/cubit/app_cubit.dart
+++ lib/cubit/app_cubit.dart
@@ -2,24 +2,37 @@ import 'dart:ffi';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mi_proyecto/dto/login_service_dto.dart';
+import 'package:mi_proyecto/service/login_service.dart';
 
 class AppState {
   final bool isUserLoggedIn;
   final bool isLoading;
   final LoginResponseDto? loginResponseDto;
 
-  AppState({required this.isUserLoggedIn, this.loginResponseDto});
+  AppState(
+      {required this.isUserLoggedIn,
+      this.loginResponseDto,
+      required this.isLoading});
 }
 
 class AppCubit extends Cubit<AppState> {
-  AppCubit() : super(AppState(isUserLoggedIn: false));
+  AppCubit() : super(AppState(isUserLoggedIn: false, isLoading: true));
 
   Void login(String username, String password) async {
     emit(AppState(isUserLoggedIn: false, isLoading: true));
     try {
-      emit(AppState(isUserLoggedIn: true, loginResponseDto: LoginResponseDto));
+      // ignore: non_constant_identifier_names
+      var LoginResponseDto = await LoginService.login(username, password);
+      emit(AppState(
+          isUserLoggedIn: true,
+          isLoading: false,
+          loginResponseDto: LoginResponseDto));
     } catch (error) {
-      emit(AppState(isUserLoggedIn: false));
+      emit(AppState(isUserLoggedIn: false, isLoading: false));
     }
+  }
+
+  void logout() {
+    emit(AppState(isUserLoggedIn: false, isLoading: false));
   }
 }
