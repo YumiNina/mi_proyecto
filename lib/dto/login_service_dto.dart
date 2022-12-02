9c0f604b6e421dbd9ef92c7a36ec1840b8d01743
mayumi nina <Mayunina55@gmail.com>
2022-12-02 09:43:58 -0400

    creacion de modulos

--- lib/dto/login_service_dto.dart
+++ lib/dto/login_service_dto.dart
@@ -1,13 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class LoginResponseDto {
   final String token;
   final String refreshToken;
   final String firstName;
   final String lastName;
-}
-LoginResponseDto (
-{
-required this.token,
-required this.refreshToken,
-required this.firstName,
-
+  LoginResponseDto({
+    required this.token,
+    required this.refreshToken,
+    required this.firstName,
+    required this.lastName,
+  });
 }
